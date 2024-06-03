35305ec79a46adbed51ce9b3176064fdb3fd2cfe
i-Blaise <menniablaise@yahoo.com>
2024-06-03 23:04:01 +0000

    favicon login

--- resources/views/dashboard/pages/auth/login.blade.php
+++ resources/views/dashboard/pages/auth/login.blade.php
@@ -6,6 +6,12 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>DOSH CMS Login</title>
+      {{-- Favicon  --}}
+      <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicon/apple-touch-icon.png') }}">
+      <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon/favicon-32x32.png') }}">
+      <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon/favicon-16x16.png') }}">
+      <link rel="manifest" href="{{ asset('images/favicon/site.webmanifest') }}">
+
   <!-- base:css -->
   <link rel="stylesheet" href="../../vendors/mdi/css/materialdesignicons.min.css">
   <link rel="stylesheet" href="../../vendors/feather/feather.css">
@@ -16,7 +22,6 @@
   <!-- inject:css -->
   <link rel="stylesheet" href="../../css/style.css">
   <!-- endinject -->
-  <link rel="shortcut icon" href="../../images/favicon.png" />
 
   {{-- Toastify  --}}
   @toastifyCss
