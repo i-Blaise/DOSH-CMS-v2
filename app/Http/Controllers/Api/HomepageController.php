daf51ac00ead9d80694a03193311203e67d65184
i-Blaise <menniablaise@yahoo.com>
2024-06-07 00:03:03 +0000

    api done

---
+++ app/Http/Controllers/Api/HomepageController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\HomeSections;
+use App\Models\Slideshow;
+use Illuminate\Http\Request;
+
+class HomepageController extends Controller
+{
+
+    public function showSlideShowData()
+    {
+        $slideshow = Slideshow::select('slideshow_image', 'caption', 'body')->get();
+        return response()->json($slideshow);
+    }
+
+    public function showHomepageSesctions(){
+        $home_sections = HomeSections::select(
+        'insurance_image',
+        'insurance_caption',
+        'insurance_body',
+        'finance_caption',
+        'finance_body',
+        'ride_image',
+        'ride_caption',
+        'ride_body',
+        'erp_image',
+        'erp_caption',
+        'erp_body',
+        'commerce_image',
+        'commerce_caption',
+        'commerce_body')->get();
+        return response()->json($home_sections);
+    }
+}
