5f18056b847397befa6e95710ae57568fde3ee96
i-Blaise <menniablaise@yahoo.com>
2025-05-16 15:15:00 +0000

    refactor: comment out debug statement in store method of HSPsController

--- app/Http/Controllers/ServiceProvidersPage/HSPsController.php
+++ app/Http/Controllers/ServiceProvidersPage/HSPsController.php
@@ -114,7 +114,7 @@ class HSPsController extends Controller
 
     public function store(Request $request)
     {
-        dd($request->all());
+        // dd($request->all());
         $validated = $request->validate([
             'hospital_name' => 'required|string|max:255',
             'country' => 'required|string|max:100',
