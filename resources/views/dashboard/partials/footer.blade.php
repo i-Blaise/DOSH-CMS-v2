80bce8c99a870605c6b94187e09259cf81e5704f
i-Blaise <menniablaise@yahoo.com>
2025-05-17 14:06:37 +0000

    fix: update footer to display current year dynamically

--- resources/views/dashboard/partials/footer.blade.php
+++ resources/views/dashboard/partials/footer.blade.php
@@ -1,6 +1,8 @@
 <footer class="footer">
     <div class="d-sm-flex justify-content-center justify-content-sm-between">
-      <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © Interactive Digital 2024</span>
+    <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">
+      Copyright © Interactive Digital {{ date('Y') }}
+    </span>
       <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> <a href="https://interactivedigital.com.gh/" target="_blank">InteractiveDigital.com.gh</a> subsidiary of Ninani Group Limited </span>
     </div>
 </footer>
