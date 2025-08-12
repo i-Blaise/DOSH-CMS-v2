839a131220ae8feac107787501b4fb1193d92db6
i-Blaise <menniablaise@yahoo.com>
2025-08-12 20:27:48 +0000

    feat: add guidance for location coordinates input format in service provider creation

--- resources/views/dashboard/pages/serviceproviders/hsp-create.blade.php
+++ resources/views/dashboard/pages/serviceproviders/hsp-create.blade.php
@@ -168,6 +168,7 @@
                         <div class="form-group">
                         <label for="location_address">Location Coodinates</label>
                         <input type="text" name="location_address" class="form-control" id="location_address" placeholder="" value="{{ old('location_address') }}">
+                        <p style="color:red; font-style:italic;">Do not leave any spaces between the cordinate or degree sign eg. <span style="color: blue">5°36'44"N 0°10'56"W</span></p>
                         </div>
 
                         <button type="submit" class="btn btn-primary me-2">Submit</button>
